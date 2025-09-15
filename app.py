2b41e146a5408039f7a126af1192bd149d8ce432
Travis Fernandes <travisf453@gmail.com>
2025-09-15 21:26:01 +0530

    Fix deployment port

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def get_bot_reply():
         return jsonify({"response": "⚠️ Server error, check console."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
